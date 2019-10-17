d18ffa03298820c37954b4e7a4ebd2363d4d2037
Sara Antoine <blythejolei@gmail.com>
2019-10-17 17:08:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,20 +2,20 @@ def introduction(name)
   puts "Hi, my name is #{name}."
  end
  
- introduction(Sara)
+ introduction(name)
  
 introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Sara, Python)
+introduction_with_language(name, language)
 
 
-introduction_with_language_optional(name,language=Ruby)
+introduction_with_language_optional(name,language="Ruby")
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
  
- introduction_with_language_optional(Sara)
+ introduction_with_language_optional(name)
 
 
 
